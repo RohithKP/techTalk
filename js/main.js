fb8aa26e3a85bf03b6b827439469e0b622be0676
RohithKP <you@example.com>
2015-12-15 13:15:43 +0530

    input border changed

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ var uzip = document.registration.zip;
 var uemail = document.registration.email;  
 var umsex = document.getElementById("male"); 
 var message = document.getElementById("message");
+// var message = document.getElementById("userid_message");
 var ufsex = document.getElementById("female"); 
 
 
@@ -154,8 +155,16 @@ return false;
 else  
 {  
 message.innerHTML ='Form Succesfully Submitted';
-window.location.reload()  
+window.location.reload(true);  
 return true;  
 }  
 }  
 
+//accept only numbers 
+function numbersonly(e){
+    var unicode=e.charCode? e.charCode : e.keyCode
+    if (unicode!=8){ //if the key isn't the backspace key (which we should allow)
+        if (unicode<48||unicode>57) //if not a number
+            return false //disable key press
+    }
+}
