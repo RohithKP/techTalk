e579b7fe57698abdefd45f271fbd3baed9b2dbcd
RohithKP <you@example.com>
2016-01-27 19:16:22 +0530

    added mouse drag

--- three/js/custom.js
+++ three/js/custom.js
@@ -6,9 +6,10 @@ var texloader;
 
 
 init();
-animate();
+// animate();
 
 function init(){
+
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight,1,1000);
     var geometry = new THREE.CylinderGeometry( 6, 6, 13, 30,30,true,0,6.3 );
@@ -18,28 +19,25 @@ function init(){
 			'img/don.jpg'
 		);
 
-console.log(tex);
 	var material = new THREE.MeshBasicMaterial(  {
 			map: tex,
-			transparent: true,
+			transparent: false,
 			depthTest: true,
-			depthWrite: false,
+			depthWrite: true,
 			polygonOffset: true,
 			polygonOffsetFactor: - 4,
 			wireframe: false
 		} );
+
 	cylinder = new THREE.Mesh(geometry, material);
 	cylinder.position.z = -20;
 	scene.add(cylinder);
 
 
-	renderer = new THREE.WebGLRenderer();
+	renderer = new THREE.WebGLRenderer({alpha:true});
 	renderer.setSize(window.innerWidth,window.innerHeight);
 	document.body.appendChild(renderer.domElement);
-
 	 window.addEventListener( 'resize', onWindowResize, false );
-
-	render();
 }
 
 function changeMaterial(x){
@@ -47,20 +45,71 @@ tex = texloader.load(x);
 cylinder.material = new THREE.MeshBasicMaterial(  {map:tex});
 }
 
-function animate(){
-	// cylinder.rotation.x += .05;
-cylinder.rotation.y += .05;
-   render();
-requestAnimationFrame( animate );
-}
-
-function render(){
-		renderer.render(scene,camera);
-}
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
     render();
+}
+
+
+//xxxx
+
+var isDragging = false;
+var previousMousePosition = {
+    x: 0,
+    y: 0
+};
+
+$(renderer.domElement).on('mousedown', function(e) {
+    isDragging = true;
+})
+.on('mousemove', function(e) {
+   
+    var deltaMove = {
+        x: e.offsetX-previousMousePosition.x,
+        y: e.offsetY-previousMousePosition.y
+    };
+    if(isDragging) {
+            
+        var deltaRotationQuaternion = new THREE.Quaternion()
+            .setFromEuler(new THREE.Euler(
+                toRadians(deltaMove.y * 1),
+                toRadians(deltaMove.x * 1),
+                0,
+                'XYZ'
+            ));
+        
+        cylinder.quaternion.multiplyQuaternions(deltaRotationQuaternion, cylinder.quaternion);
+    }
+    
+    previousMousePosition = {
+        x: e.offsetX,
+        y: e.offsetY
+    };
+});
+/* */
+
+$(document).on('mouseup', function(e) {
+    isDragging = false;
+});
+
+
+function render() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+render();
+// update(0, totalGameTime);
+
+
+
+
+function toRadians(angle) {
+	return angle * (Math.PI / 180);
+}
+
+function toDegrees(angle) {
+	return angle * (180 / Math.PI);
 }
